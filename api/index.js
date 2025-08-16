1864cdcd1c38ea50f6ea33f95901b89cb2641cd8
Zahab Jahangir <zahabjahangir27@gmail.com>
2025-08-17 03:01:11 +0500

    mongo debugging

--- api/index.js
+++ api/index.js
@@ -8,9 +8,13 @@ app.use(cors());
 app.use(express.json());
 
 //db connection
-mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI, {
+  serverSelectionTimeoutMS: 5000,
+  autoReconnect: true,
+}).then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error('MongoDB Error:', err.message));
 
+  
 app.use('/api/users', require('../routes/userRoutes'));
 
 module.exports = app;
