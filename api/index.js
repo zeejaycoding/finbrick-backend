9817497113f85f55967f66ee8c3b2feea3ee198c
Zahab Jahangir <zahabjahangir27@gmail.com>
2025-08-17 03:52:06 +0500

    chnage in index.js

--- api/index.js
+++ api/index.js
@@ -1,19 +1,20 @@
 require('dotenv').config();
-const express=require('express');
-const mongoose=require('mongoose');
-const cors=require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-const app=express();
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-//db connection
 mongoose.connect(process.env.MONGO_URI, {
-  serverSelectionTimeoutMS: 5000, // 5-second timeout
+  serverSelectionTimeoutMS: 5000,
 })
   .then(() => console.log('MongoDB Connected successfully'))
-  .catch(err => console.error('MongoDB Connection Error:', err.message));
+  .catch(err => console.error('MongoDB Connection Error:', err.message, 'from IP:', req?.headers['x-forwarded-for'] || 'Unknown'));
 
 app.use('/api/users', require('../routes/userRoutes'));
+
+app.get('/', (req, res) => res.send('Finbrick Backend API is running'));
 
 module.exports = app;
